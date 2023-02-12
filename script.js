e489f1dc46b25881b455dccd7e07c8b36203ff48
jameshortonthird <jameshortonthird@gmail.com>
2023-02-12 18:21:10 -0500

    added event listener to start button and created var in JS for first elements in html

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 // var apikey = 339ca470cb3565cbcc94ce92292b13d9
+var cityText = document.getElementById("cityText")
+var searchButton = document.getElementById("searchButton")
+var savedCityText = document.getElementById("savedCityText")
+var currentDayCard = document.getElementById("currentDayCard")
+
+searchButton.addEventListener("click", searchCity)
+
+function searchCity(event) {
+    event.preventDefault();
+    
+    var nameOfCity = cityText.value.trim();
+    
+
+    if (nameOfCity) {
+        saveCity();
+        findCity(nameOfCity);
+        console.log(nameOfCity);
+    }
+}
+
+
+
+
+// function formatCurrentCard(weather) {
+//     console.log ("Today's Weather")
+//     console.log (weather);
+    
+//     currentDayCard.innerHTML = "";
+
+//     currentDayCard.innerHTML += `
+//     <div class="card-body">
+//     <p class="card-text"> $`
+// }
